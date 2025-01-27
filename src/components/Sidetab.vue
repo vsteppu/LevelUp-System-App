f7403ff3896367e1653ee2545bb075e8b4065a10
Your Name <vlad@wonsulting.com>
2025-01-27 11:36:43 +0200

    v0.0.4

--- src/components/Sidetab.vue
+++ src/components/Sidetab.vue
@@ -5,9 +5,10 @@
     <p @click="openSidetab = !openSidetab" v-if="openSidetab" class="pi pi-times text-2xl mr-2 p-2 absolute right-0"></p>
   </div>
 
-  <nav class="font-light flex flex-col ml-6 h-screen my-5" v-if="openSidetab">
+  <nav class="font-light flex flex-col ml-6 h-screen my-5" @click="openSidetab = false" v-if="openSidetab">
     <router-link to="/" class=" w-40 uppercase my-3 ml-4">Daily Quest</router-link>
     <router-link to="/quests" class=" w-40 uppercase my-3 ml-4">Quests</router-link>
+    <router-link to="/player" class=" w-40 uppercase my-3 ml-4">Player</router-link>
     <!-- <router-link to="/player" ><PLayerIcon/></router-link> -->
   </nav>
   <hr class=" border-blue-300 mb-3 md:hidden " />
