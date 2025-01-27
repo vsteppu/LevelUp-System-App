69ea5a999114c86aa4408b079c0a7ed9c5a85cff
Your Name <vlad@wonsulting.com>
2025-01-27 11:19:31 +0200

    v.0.0.3

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,47 @@
     <div class="h-8 w-[0.5px] bg-blue-300"></div>
     <router-link to="/quests" class="h-16 w-40 flex justify-center items-center uppercase">Quests</router-link>
     <div class="h-8 w-[0.5px] bg-blue-300"></div>
+    <router-link to="/player" class="h-16 w-40 flex justify-center items-center uppercase">Player</router-link>
     <!-- <router-link to="/player" ><PLayerIcon/></router-link> -->
   </nav>
   <router-view></router-view>
+  <button @click="userStatus()"></button>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
+import { supabase } from '../src/supabase.js'
+import { useAuthStore } from '@/stores/authStore.js';
 
 import PLayerIcon from './components/details/PLayerIcon.vue';
 import Sidetad from './components/Sidetab.vue';
+import router from '../src/router/index.js';
+
+const authStore = useAuthStore()
 
 const isFirstRun = ref(false);
 const openSidetab = ref(false);
 
+const playerStatus = ref('')
+
+const userStatus = async () => {
+  const user = await authStore.getUser()
+  playerStatus.value = user
+  if (playerStatus.value === null){
+        router.push('/auth')
+      }
+}
+
+const isAuthenticaded = computed(()=>playerStatus.value)
+
 onMounted(() => {
+  userStatus()
+  watch(()=>playerStatus,
+    ()=>{
+      if (isAuthenticaded === null){
+        router.push('/auth')
+      }
+    })
   // Check if this is the first time the user is visiting
   if (!localStorage.getItem('hasVisited')) {
     isFirstRun.value = true;
--- src/views/LoginPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <StartNotification/>
-</template>
-
-<script setup>
-import StartNotification from '../components/StartNotification.vue'
-</script>
